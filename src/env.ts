a5b27d0f631410e22a966f9d9ec8c39ee004aa49
tomfln <tomfln@gmx.de>
2026-02-14 00:47:04 +0100

    feat: add example docker compose file and clean up env vars

--- src/env.ts
+++ src/env.ts
@@ -6,8 +6,6 @@ const envSchema = z.object({
   TOKEN: z.string().min(1),
   CLIENT_ID: z.string().min(1),
   W2G_KEY: z.string().min(1),
-  SPOTIFY_CLIENT_ID: z.string().min(1),
-  SPOTIFY_SECRET: z.string().min(1),
   DATA_DIR: z.string().default("./")
     .transform(dir => resolve(import.meta.dir, "..", dir)),
 })
